b83222f42e58e87ea49997eb8c18b83d5205b22e
farhana-yeasmin-sebpo <farhana.sebpo@gmail.com>
2023-01-16 21:50:53 +0600

    first commit

---
+++ Day 1/comment.py
@@ -0,0 +1,11 @@
+# ctrl+ forward slash for multiline comment
+# This is a comment
+# written in
+# more than just one line
+
+print("Hello, World!")
+
+
+"""
+Multi line comment using triple quote
+"""
---
+++ Day 1/variable.py
@@ -0,0 +1,24 @@
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+# print(x)
+# print(y)
+# print(z)
+
+x = 5
+y = "John"
+print(type(x)) # int
+print(type(y)) # str
+
+
+# String assigning using double or single quote
+x = "John"
+# is the same as
+x = 'John'
+print(x)
+
+
+a = 4
+A = "Sally"
+#A will not overwrite a
+print(a)
